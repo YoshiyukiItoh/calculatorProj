adecbb8dfb262c7d066d275938ef9e7f0cb27c94
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2014-12-31 21:45:22 +0900

    Initial Commit

---
+++ calculatorProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculatorProj
+//
+//  Created by Yoshi on 2014/12/31.
+//  Copyright (c) 2014年 Yoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calculatorProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calculatorProj
+//
+//  Created by Yoshi on 2014/12/31.
+//  Copyright (c) 2014年 Yoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
