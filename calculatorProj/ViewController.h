c0656b755752920e9c789b8444d81da1aec15028
Yoshiyuki Itoh <swallows.vct1a19@gmail.com>
2015-01-02 00:17:09 +0900

    紐付けを行った。

--- calculatorProj/ViewController.h
+++ calculatorProj/ViewController.h
@@ -10,6 +10,43 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *calcResult;
+
+@property (weak, nonatomic) IBOutlet UIButton *button0;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+@property (weak, nonatomic) IBOutlet UIButton *button9;
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonDot;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEqual;
+@property (weak, nonatomic) IBOutlet UIButton *buttonPlus;
+@property (weak, nonatomic) IBOutlet UIButton *buttonMinus;
+@property (weak, nonatomic) IBOutlet UIButton *buttonMulti;
+@property (weak, nonatomic) IBOutlet UIButton *buttonDivide;
+
+- (IBAction)click0:(id)sender;
+- (IBAction)click1:(id)sender;
+- (IBAction)click2:(id)sender;
+- (IBAction)click3:(id)sender;
+- (IBAction)click4:(id)sender;
+- (IBAction)click5:(id)sender;
+- (IBAction)click6:(id)sender;
+- (IBAction)click7:(id)sender;
+- (IBAction)click8:(id)sender;
+- (IBAction)click9:(id)sender;
+- (IBAction)clickDot:(id)sender;
+- (IBAction)clickEqu:(id)sender;
+- (IBAction)clickPlus:(id)sender;
+- (IBAction)clickMinus:(id)sender;
+- (IBAction)clickMulti:(id)sender;
+- (IBAction)clickDivide:(id)sender;
+
 
 @end
 
--- calculatorProj/ViewController.m
+++ calculatorProj/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ViewController ()
 
@@ -16,12 +17,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.s
 }
 
+- (IBAction)click0:(id)sender {
+}
+
+- (IBAction)click1:(id)sender {
+}
+
+- (IBAction)click2:(id)sender {
+}
+
+- (IBAction)click3:(id)sender {
+}
+
+- (IBAction)click4:(id)sender {
+}
+
+- (IBAction)click5:(id)sender {
+}
+
+- (IBAction)click6:(id)sender {
+}
+
+- (IBAction)click7:(id)sender {
+}
+
+- (IBAction)click8:(id)sender {
+}
+
+- (IBAction)click9:(id)sender {
+}
+
+- (IBAction)clickDot:(id)sender {
+}
+
+- (IBAction)clickEqu:(id)sender {
+}
+
+- (IBAction)clickPlus:(id)sender {
+}
+
+- (IBAction)clickMinus:(id)sender {
+}
+
+- (IBAction)clickMulti:(id)sender {
+}
+
+- (IBAction)clickDivide:(id)sender {
+}
 @end
